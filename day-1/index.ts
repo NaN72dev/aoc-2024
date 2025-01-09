c6d98c112407e243a7fc0780a71e5d00958acaa9
Nan72 <lecongnhan29@gmail.com>
2025-01-09 12:10:47 +0700

    feat(day1): completed day 1 part 2

--- day-1/index.ts
+++ day-1/index.ts
@@ -1,11 +1,24 @@
 import * as fs from "fs";
+import {exit} from "process";
+
+enum Mode {
+    DISTANCE = "DISTANCE",
+    SIMILARITY = "SIMILARITY"
+}
 
 const [, , ...args] = process.argv;
-let INPUT_FILE_PATH;
-if (args.length !== 1)
-    INPUT_FILE_PATH = "day-1/input";
-else
-    INPUT_FILE_PATH = args[0];
+if (args.length < 2) {
+    console.log(`Usage: node day-1/index.js <${Object.values(Mode).join("|")}> <input-file-path>`);
+    exit(1);
+}
+
+const MODE = args[0] as Mode;
+if (!Object.values(Mode).includes(MODE)) {
+    console.error("Invalid mode ", MODE);
+    exit(1);
+}
+
+const INPUT_FILE_PATH = args[1] || "day-1/input";
 
 const SEPARATOR = "   ";
 
@@ -23,18 +36,46 @@ let {first, second} = input.split("\n")
         return acc;
     }, {first: [] as number[], second: [] as number[]});
 
-// sort the arrays in ascending order
-const sortFn = (a: number, b: number) => a - b;
-first = first.sort(sortFn);
-second = second.sort(sortFn);
-
-if (first.length !== second.length) {
+if (first.length !== second.length)
     throw new Error("Arrays are not of the same length");
+
+function distance(first: number[], second: number[]) {
+    // sort the arrays in ascending order
+    const sortFn = (a: number, b: number) => a - b;
+    first = first.sort(sortFn);
+    second = second.sort(sortFn);
+
+    // find the difference between the two arrays
+    return first.reduce(
+        (acc, curr, index) => acc + Math.abs(curr - second[index]),
+        0
+    );
 }
 
-// find the difference between the two arrays
-const difference = first.reduce(
-    (acc, curr, index) => acc + Math.abs(curr - second[index]),
-    0
-);
-console.log(difference);
+function similarity(first: number[], second: number[]) {
+    const countMap = second.reduce((acc, curr) => {
+        const count = acc.get(curr);
+        if (count)
+            acc.set(curr, count + 1);
+        else
+            acc.set(curr, 1);
+
+        return acc;
+    }, new Map<number, number>());
+
+    return first.reduce((acc, curr) => {
+        const count = countMap.get(curr);
+        if (!count) return acc;
+
+        return acc + curr * count;
+    }, 0);
+}
+
+switch (MODE) {
+    case Mode.DISTANCE:
+        console.log(MODE, distance(first, second));
+        break;
+    case Mode.SIMILARITY:
+        console.log(MODE, similarity(first, second));
+        break;
+}
