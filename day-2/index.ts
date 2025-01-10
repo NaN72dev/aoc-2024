a5e5555512085d07d3981cb8460b4f0087a205ae
Nan72 <lecongnhan29@gmail.com>
2025-01-10 14:05:40 +0700

    ref(day2): prepare for part 2

--- day-2/index.ts
+++ day-2/index.ts
@@ -1,21 +1,56 @@
 import {exit} from "process";
 import * as fs from "fs";
 
-const ARGS_COUNT = 1;
+const ARGS_COUNT = 2;
 const SEPARATOR = " ";
 const MIN_LEVEL_DIFF = 1;
 const MAX_LEVEL_DIFF = 3;
 
 const [, , ...args] = process.argv;
-if (args.length < ARGS_COUNT) {
-    console.log(`Usage: node day-2/index.js <input-file-path>`);
+if (args.length < 1) {
+    console.log(`Usage: node day-2/index.js <input-file-path> [tolerate]`);
     exit(1);
 }
 
 const INPUT_FILE_PATH = args[0];
 if (!INPUT_FILE_PATH) {
-    console.log(`Usage: node day-2/index.js <input-file-path>`);
+    console.log(`Usage: node day-2/index.js <input-file-path> [tolerate]`);
     exit(1);
+}
+const IS_TOLERATE = args[1] !== "tolerate";
+
+console.info(`${IS_TOLERATE ? "not " : ""}tolerated`);
+
+function isSafe(levels: number[]): boolean {
+    const diffs = levels.reduce((acc, curr, index) => {
+        if (index <= 0) return acc;
+        if (acc.hasZero) return acc;
+
+        const prev = levels[index - 1];
+        const diff = prev - curr;
+        // console.debug("diff", {index, prev, curr, diff});
+
+        if (diff === 0) {
+            acc.hasZero = true;
+            return acc;
+        }
+
+        acc.maxDiff = Math.max(acc.maxDiff, diff);
+        acc.minDiff = Math.min(acc.minDiff, diff);
+
+        return acc;
+    }, {maxDiff: -Number.MAX_VALUE, minDiff: Number.MAX_VALUE, hasZero: false});
+    // console.debug("report", {report, diffs});
+
+    // there are no increasing or decreasing diffs
+    if (diffs.hasZero) return false;
+
+    // there are increasing diffs and decreasing diffs
+    if (diffs.maxDiff * diffs.minDiff <= 0) return false;
+
+    // out of bounds diff
+    if (Math.max(Math.abs(diffs.maxDiff), Math.abs(diffs.minDiff)) > MAX_LEVEL_DIFF) return false;
+    return Math.min(Math.abs(diffs.maxDiff), Math.abs(diffs.minDiff)) >= MIN_LEVEL_DIFF;
 }
 
 const res = fs.readFileSync(INPUT_FILE_PATH, "utf8")
@@ -23,38 +58,9 @@ const res = fs.readFileSync(INPUT_FILE_PATH, "utf8")
     .reduce((acc, report) => {
         const levels = report.split(SEPARATOR)
             .map(Number);
+        if (!isSafe(levels)) return acc;
 
-        const diffs = levels.reduce((acc, curr, index) => {
-            if (index <= 0) return acc;
-            if (acc.hasZero) return acc;
-
-            const prev = levels[index - 1];
-            const diff = prev - curr;
-            console.debug("diff", {index, prev, curr, diff});
-
-            if (diff === 0) {
-                acc.hasZero = true;
-                return acc;
-            }
-
-            acc.maxDiff = Math.max(acc.maxDiff, diff);
-            acc.minDiff = Math.min(acc.minDiff, diff);
-
-            return acc;
-        }, {maxDiff: -Number.MAX_VALUE, minDiff: Number.MAX_VALUE, hasZero: false});
-        console.debug("report", {report, diffs});
-
-        // there are no increasing or decreasing diffs
-        if (diffs.hasZero) return acc;
-
-        // there are increasing diffs and decreasing diffs
-        if (diffs.maxDiff * diffs.minDiff <= 0) return acc;
-
-        // out of bounds diff
-        if (Math.max(Math.abs(diffs.maxDiff), Math.abs(diffs.minDiff)) > MAX_LEVEL_DIFF) return acc;
-        if (Math.min(Math.abs(diffs.maxDiff), Math.abs(diffs.minDiff)) < MIN_LEVEL_DIFF) return acc;
-
-        console.info("report", report, "is safe!");
+        // console.info("report", report, "is safe!");
         return acc + 1;
     }, 0);
 
